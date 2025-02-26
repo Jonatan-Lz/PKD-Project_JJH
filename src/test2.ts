1ae044ac0fbedfd22b69a0cdb5ed4c951adadf49
Hjalmar <hjalmar.bostrom.6244@student.uu.se>
2025-02-26 16:58:00 +0100

    Added basic graphics and stuff

---
+++ src/test2.ts
@@ -0,0 +1,21 @@
+import { createMap, drawMap, printer} from "./test.js";
+
+let x = 0;
+let y = 0;
+const a = createMap();
+function handleKeyEvent(event: KeyboardEvent): void {
+    const key = event.key;
+    if(key === "d"){
+        x++;
+    } else if(key === "a"){
+        x--;
+    } else if(key === "w"){
+        y++;
+    } else if(key === "s"){
+        y--;
+    }
+    drawMap(a, 5, 5, 2, x, y);
+    const output = document.getElementById('output') as HTMLParagraphElement; 
+    output.innerHTML = printer(a);
+}
+document.addEventListener('keydown', handleKeyEvent);
