c5ce3f61aa33abf57c98fb5f204803d3b180db73
Jonatan Lantz <jola0878@student.uu.se>
2025-02-27 13:38:25 +0100

    added comments and changed drawScreen to not use x and y values, instead take them from ship directly.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createScreen, drawScreen, printer} from "./drawScreen.js";
 import { createPlanet } from "./planet.js";
 import { createShip } from "./ship.js";
 import { change_location, get_x, get_y, planet, ship } from "./types.js";
+
 const screen = createScreen();
 const planet_list: List<planet> = list(createPlanet(3, 4, 4),createPlanet(3, 35, 6),createPlanet(6, 14, 18));
 let playerShip: ship = createShip(1, 0, 0, "A", 200);
@@ -17,7 +18,7 @@ function handleKeyEvent(event: KeyboardEvent): void {
     } else if(key === "s"){
         change_location(playerShip, get_x(playerShip), get_y(playerShip) + 1);
     }
-    drawScreen(screen, planet_list, get_x(playerShip), get_y(playerShip), playerShip);
+    drawScreen(screen, planet_list, playerShip);
     const output = document.getElementById('output') as HTMLParagraphElement; 
     output.innerHTML = printer(screen);
 }
