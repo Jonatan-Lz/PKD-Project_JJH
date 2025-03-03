d42f9394b724af1d59aa8a87f3e2783a604a8b3b
Jonatan Lantz <jola0878@student.uu.se>
2025-03-03 09:28:11 +0100

    merged branch hjalmar to jonatan. Also compiled.

--- dist/src/types.js
+++ dist/src/types.js
@@ -1,2 +1,17 @@
-export {};
-//team true is friendly, false is enemy. lifeTime is ticks left until despawn
+import { head, pair, tail } from "../lib/list.js";
+//gets x-coordinate of object
+export function get_x(object) {
+    return head(object.gameObject.location);
+}
+//gets y-coordinate of object
+export function get_y(object) {
+    return tail(object.gameObject.location);
+}
+//move object to given coordinates
+export function change_location(object, x, y) {
+    object.gameObject.location = pair(x, y);
+}
+//get sprite of object
+export function get_sprite(object) {
+    return object.gameObject.sprite;
+}
